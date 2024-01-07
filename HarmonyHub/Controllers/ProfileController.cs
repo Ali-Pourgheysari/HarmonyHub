334618ee9648bbed7e88ea0b0942762b9f98f59d
Ali Pourgheysari <a.geysar@gmail.com>
2024-01-07 21:40:50 +0330

    Update ProfileController.cs

--- HarmonyHub/Controllers/ProfileController.cs
+++ HarmonyHub/Controllers/ProfileController.cs
@@ -1,83 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HarmonyHub.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HarmonyHub.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly IUserService userService;
+
+        public ProfileController(IUserService userService)
+        {
+            this.userService = userService;
+        }
         // GET: ProfileController
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: ProfileController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Edit()
         {
             return View();
-        }
-
-        // GET: ProfileController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ProfileController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProfileController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProfileController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProfileController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProfileController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
