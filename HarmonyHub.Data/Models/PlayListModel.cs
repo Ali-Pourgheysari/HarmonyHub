af6baa201d286415c2eeb92ec0f770f0e65a4adc
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-07 15:25:41 +0330

    Create Playlist Model

---
+++ HarmonyHub.Data/Models/PlayListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Data.Models
+{
+    public class PlayListModel
+    {
+        public string? UserName { get; set; }
+        public ICollection<SongModel> Songs { get; set; } = new List<SongModel>();
+    }
+}
