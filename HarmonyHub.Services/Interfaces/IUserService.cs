4bc1b59701a5281e81452e3fc23ef9b34ad3fba0
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-07 13:47:45 +0330

    Added user service

---
+++ HarmonyHub.Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyHub.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> GetUserByIdAsync(string id);
+        Task<User> GetUserByEmailAsync(string email);
+    }
+}
---
+++ HarmonyHub.Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyHub.Data;
+using HarmonyHub.Data.Entities;
+using HarmonyHub.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly HarmonyHubDbContext dbContext;
+        public UserService(HarmonyHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await dbContext.Users
+                .Include(u => u.FollowingArtists)
+                    .ThenInclude(fa => fa.Artist)
+                .Include(u => u.PlayList)
+                    .ThenInclude(pl => pl.Songs)
+                        .ThenInclude(s => s.Song)
+                            .ThenInclude(s => s.Artists)
+                .FirstAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await dbContext.Users
+                .Include(u => u.FollowingArtists)
+                    .ThenInclude(fa => fa.Artist)
+                .Include(u => u.PlayList)
+                    .ThenInclude(pl => pl.Songs)
+                        .ThenInclude(s => s.Song)
+                            .ThenInclude(s => s.Artists)
+                .FirstAsync(u => u.Id == id);
+        }
+    }
+}
