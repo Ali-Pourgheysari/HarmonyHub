ddac02cbb3cca8081db4586024680fe05bf35ff8
Ali Pourgheysari <a.geysar@gmail.com>
2024-01-07 23:24:35 +0330

    Create EditProfileModel.cs

---
+++ HarmonyHub.Data/Models/EditProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Data.Models
+{
+    public class EditProfileModel
+    {
+        public string Id { get; set; }
+        public string? FirstName{ get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
