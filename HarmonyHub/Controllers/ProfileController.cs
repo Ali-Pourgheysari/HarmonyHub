bdfe39eec2c2a60d5faa65fff46177396da9af58
Ali Pourgheysari <a.geysar@gmail.com>
2024-01-08 00:37:41 +0330

    Add ability to edit profile

--- HarmonyHub/Controllers/ProfileController.cs
+++ HarmonyHub/Controllers/ProfileController.cs
@@ -1,8 +1,12 @@
+
 ﻿using HarmonyHub.Data.EntityMappings;
 using HarmonyHub.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+﻿using HarmonyHub.Data.Models;
+using HarmonyHub.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using HarmonyHub.Data.Entities;
 
 namespace HarmonyHub.Controllers
 {
@@ -28,9 +32,34 @@ namespace HarmonyHub.Controllers
             return NotFound();
         }
 
-        public ActionResult Edit()
+        public async Task<ActionResult> Edit()
         {
-            return View();
+            var currentUserEmail = User.Identity.Name;
+            var user = await userService.GetUserByEmailAsync(currentUserEmail.ToString());
+            EditProfileModel profile = new EditProfileModel()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+            };
+            return View(profile);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<ActionResult> Edit(EditProfileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userService.GetUserByIdAsync(model.Id);
+                int changed = await userService.UpdateUserAsync(user.UpdateFields(model));
+                if (changed > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
         }
     }
 }
