a756c84880942bc85de98a200d3e2b37950cce66
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-08 14:17:57 +0330

    Create PlayListUtility.cs

---
+++ HarmonyHub.Data/Utilities/PlayListUtility.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyHub.Data.Entities;
+using HarmonyHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Data.Utilities
+{
+	public static class PlayListUtility
+	{
+		public static List<SongModel> MarkPlayListSongs(this List<SongModel> songs, User user)
+		{
+            if (user != null)
+			{
+				// create a list of playlist song ids
+				var playListSongIds = user.PlayList.Songs.Select(s => s.SongId).ToList();
+				// for each song in the artist's songs
+				foreach (var song in songs)
+				{
+						// check if the song id is in the list
+						song.InPLayList = playListSongIds.Contains(song.Id);
+				}
+            }
+			return songs;
+		}
+	}
+}
