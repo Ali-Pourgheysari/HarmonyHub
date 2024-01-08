d0552bcc9ec3a2765f8564e0e955f838b93677ca
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-08 14:17:51 +0330

    Create FollowingArtistUtility.cs

---
+++ HarmonyHub.Data/Utilities/FollowingArtistUtility.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyHub.Data.Entities;
+using HarmonyHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Data.Utilities
+{
+	public static class FollowingArtistUtility
+	{
+		public static ArtistModel MarkFollowedArtist(this ArtistModel artist, User user)
+		{
+			if (user != null)
+			{
+				// create a list of followed artist ids
+				var followedArtistIds = user.FollowingArtists.Select(a => a.ArtistId).ToList();
+				// check if the artist id is in the list
+				artist.IsFollowed = followedArtistIds.Contains(artist.Id);
+			}
+			return artist;
+		}
+		public static List<ArtistModel> MarkFollowedArtists(this List<ArtistModel> artists, User user)
+		{
+			if (user != null)
+			{
+				// create a list of followed artist ids
+				var followedArtistIds = user.FollowingArtists.Select(a => a.ArtistId).ToList();
+				// for each artist in the list of artists
+				foreach (var artist in artists)
+				{
+					// check if the artist id is in the list
+					artist.IsFollowed = followedArtistIds.Contains(artist.Id);
+				}
+			}
+			return artists;
+		}
+	}
+}
