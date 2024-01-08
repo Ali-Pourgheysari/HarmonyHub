6304d210229615ca8d38ef8bd955eaadfa001826
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-08 14:18:28 +0330

    added search

---
+++ HarmonyHub.Data/Models/SearchResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyHub.Data.Models
+{
+    public class SearchResultModel
+    {
+        public string Query { get; set; }
+        public List<SongModel> Songs { get; set; } = new();
+        public List<ArtistModel> Artists { get; set; } = new();
+    }
+}
---
+++ HarmonyHub/Controllers/SearchController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HarmonyHub.Data.Models;
+using HarmonyHub.Services.Interfaces;
+using HarmonyHub.Data.EntityMappings;
+using HarmonyHub.Data.Utilities;
+
+namespace HarmonyHub.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ISongService songService;
+        private readonly IArtistService artistService;
+        private readonly IUserService userService;
+
+        public SearchController(
+            ISongService songService,
+            IArtistService artistService,
+            IUserService userService
+            )
+        {
+            this.songService = songService;
+            this.artistService = artistService;
+            this.userService = userService;
+        }
+        // GET: Search?query=foo
+        public async Task<ActionResult> Index(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+				return View(new SearchResultModel());
+			}
+
+            var artist = await artistService.SearchArtistByString(query);
+            var songs = await songService.SearchSongByString(query);
+            var model = new SearchResultModel()
+            {
+                Query = query,
+                Artists = artist.ToArtistModels(),
+                Songs = songs.ToSongModels()
+            };
+
+            var username = User.Identity.Name;
+            if (username != null)
+            {
+				var user = await userService.GetUserByEmailAsync(username);
+                if (user != null)
+                {
+					model.Artists = model.Artists.MarkFollowedArtists(user);
+					model.Songs = model.Songs.MarkPlayListSongs(user);
+				}
+			}
+
+            return View(model);
+        }
+    }
+}
