c388a5132d856fe261c744f60e9cf33b07c368c5
Alireza Karimi <alireza.mathers@gmail.com>
2024-01-18 18:25:52 +0330

    email change fixed

--- HarmonyHub/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ HarmonyHub/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -127,7 +127,7 @@ namespace HarmonyHub.Areas.Identity.Pages.Account.Manage
                 await _emailSender.SendEmailAsync(
                     Input.NewEmail,
                     "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl).Replace("&amp;", "&")}'>clicking here</a>.");
 
                 StatusMessage = "Confirmation link to change email sent. Please check your email.";
                 return RedirectToPage();
